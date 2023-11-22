f6f9b32503404c07e5146816b1d95e588081e33f
Miguel Angelo <migueltiojhoe@hotmail.com>
2023-11-22 13:01:22 -0300

    classe calculadora com dois metodos

---
+++ src/Calculadora.java
@@ -0,0 +1,23 @@
+public class Calculadora {
+
+    // Método para somar dois números
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    // Método para subtrair dois números
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        // Exemplo de uso da classe
+        Calculadora calculadora = new Calculadora();
+
+        int resultadoSoma = calculadora.somar(5, 3);
+        System.out.println("Resultado da soma: " + resultadoSoma);
+
+        int resultadoSubtracao = calculadora.subtrair(10, 4);
+        System.out.println("Resultado da subtração: " + resultadoSubtracao);
+    }
+}
